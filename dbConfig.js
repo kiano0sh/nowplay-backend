dfb7f54b10efd7b9d26c355d4223c32d6067eea9
kianoosh hooshmand <kianoosh.hooshmand@gmail.com>
2019-05-13 21:08:05 +0430

    dbConfig, package-lock added

---
+++ dbConfig.js
@@ -0,0 +1,14 @@
+const {Client} = require('pg')
+
+
+const client = new Client({
+    database: "prisma",
+    user: "prisma",
+    password: "prisma",
+    host: "localhost",
+    port: 3001
+})
+
+module.exports = {
+    client,
+};
